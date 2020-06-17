53239442f47081fa3a62a798981b770b1efa86ad
Ling CHU <meetchuling@gmail.com>
2020-06-17 14:07:15 +0800

    remove unsafe code, use gc

--- src/tree/mod.rs
+++ src/tree/mod.rs
@@ -7,105 +7,88 @@ mod node;
 use node::Node;
 
 use std::cmp::Eq;
+use std::fmt::Debug;
 use std::hash::Hash;
-use std::usize;
 
 #[derive(Debug)]
 pub struct TrieTree<T: Hash + Eq + Clone> {
     root: Box<Node<T>>,
+    is_dirty: bool,
 }
 
 impl<T: Hash + Eq + Clone> TrieTree<T> {
     pub fn new() -> TrieTree<T> {
         TrieTree {
             root: Box::new(Node::new()),
+            is_dirty: false,
         }
     }
 
-    pub fn insert(&mut self, seq: &[T]) {
+    pub fn insert(&mut self, seq: &[T]) -> bool {
         if seq.len() == 0 {
-            return;
+            return false;
         }
-        let mut curr_node: *mut Node<T> = &mut *self.root;
-        unsafe {
-            for ch in seq.iter() {
-                if !(*curr_node).contains_key(ch) {
-                    (*curr_node).key_alloc(ch);
-                }
-                curr_node = (*curr_node).key_next_mut(ch);
-            }
-            (*curr_node).mark();
-        }
-    }
-
-    pub fn remove(&mut self, seq: &[T]) {
-        if seq.len() == 0 {
-            return;
-        }
-        if let Some(t_node) = self.get_prefix_end_mut(seq) {
-            unsafe {
-                if (*t_node).is_a_seq() {
-                    (*t_node).unmark();
-                    if !(*t_node).is_a_seq() {
-                        let has_alive_child = || -> bool {
-                            // dfs
-                            let mut to_visit = vec![t_node];
-                            while let Some(exp_node) = to_visit.pop() {
-                                to_visit.extend(
-                                    (*exp_node).get_all_leaves_mut().into_iter().map(|(_, v)| v),
-                                );
-                                if (*exp_node).cannot_be_deleted() {
-                                    return true;
-                                }
-                            }
-                            false
-                        };
-                        if has_alive_child() {
-                            return;
-                        }
-                        // delete
-                        let mut curr_node: *mut Node<T> = &mut *self.root;
-                        let mut last_alive_node = curr_node;
-                        let mut to_del_ch = &seq[0];
-                        for ch in seq.iter() {
-                            if (*curr_node).cannot_be_deleted() {
-                                last_alive_node = curr_node;
-                                to_del_ch = ch;
-                            }
-                            curr_node = (*curr_node).key_next_mut(ch);
-                        }
-                        (*last_alive_node).key_destroy(to_del_ch);
-                    }
-                }
-            }
-        }
-    }
-
-    fn get_prefix_end(&self, seq: &[T]) -> Option<*const Node<T>> {
-        assert!(seq.len() != 0);
-        let mut curr_node: *const Node<T> = &*self.root;
+        let mut curr_node = &mut self.root;
         for ch in seq.iter() {
-            unsafe {
-                if (*curr_node).contains_key(ch) {
-                    curr_node = (*curr_node).key_next(ch);
-                } else {
-                    return None;
-                }
+            if !curr_node.contains_key(ch) {
+                curr_node.key_alloc(ch);
+            }
+            curr_node = curr_node.key_next_mut(ch);
+        }
+        curr_node.mark()
+    }
+
+    pub fn remove(&mut self, seq: &[T]) -> bool {
+        if seq.len() == 0 {
+            return false;
+        }
+        let r = if let Some(t_node) = self.get_prefix_end_mut(seq) {
+            t_node.unmark()
+        } else {
+            false
+        };
+        if r {
+            self.is_dirty = true;
+        }
+        r
+    }
+
+    pub fn requires_gc(&self) -> bool {
+        self.is_dirty
+    }
+
+    pub fn gc(&mut self) -> bool {
+        if !self.requires_gc() {
+            return false;
+        }
+        // --- gc start ---
+        // TODO
+        // --- gc end ---
+        self.is_dirty = false;
+        true
+    }
+
+    fn get_prefix_end(&self, seq: &[T]) -> Option<&Box<Node<T>>> {
+        assert!(seq.len() != 0);
+        let mut curr_node = &self.root;
+        for ch in seq.iter() {
+            if curr_node.contains_key(ch) {
+                curr_node = curr_node.key_next(ch);
+            } else {
+                return None;
             }
         }
         Some(curr_node)
     }
 
-    fn get_prefix_end_mut(&mut self, seq: &[T]) -> Option<*mut Node<T>> {
+    fn get_prefix_end_mut(&mut self, seq: &[T]) -> Option<&mut Box<Node<T>>> {
         assert!(seq.len() != 0);
-        let mut curr_node: *mut Node<T> = &mut *self.root;
+        let mut curr_node = &mut self.root;
         for ch in seq.iter() {
-            unsafe {
-                if (*curr_node).contains_key(ch) {
-                    curr_node = (*curr_node).key_next_mut(ch);
-                } else {
-                    return None;
-                }
+            if curr_node.contains_key(ch) {
+                curr_node = curr_node.key_next_mut(ch);
+            } else {
+                return None;
             }
         }
         Some(curr_node)
@@ -118,7 +101,7 @@ impl<T: Hash + Eq + Clone> TrieTree<T> {
         self.get_prefix_end(seq).is_some()
     }
 
-    pub fn prefix_match_maxn<'a>(&self, seq: &'a [T], maxn: usize) -> Option<Vec<Vec<&'a T>>> {
+    pub fn prefix_match_maxn<'a>(&'a self, seq: &'a [T], maxn: usize) -> Option<Vec<Vec<&'a T>>> {
         if seq.len() == 0 {
             return None;
         }
@@ -129,24 +112,20 @@ impl<T: Hash + Eq + Clone> TrieTree<T> {
             let mut r = vec![];
             let mut tail_seq: Vec<&T> = vec![];
             // dfs
-            unsafe {
-                let mut to_visit = vec![Some((seq.last().unwrap(), node))];
-                while let Some(record) = to_visit.pop() {
-                    if let Some((ch, leaf)) = record {
-                        to_visit.push(None);
-                        to_visit.extend((*leaf).get_all_leaves().into_iter().map(|v| Some(v)));
-                        tail_seq.push(ch);
-                        if (*leaf).is_a_seq() {
-                            r.push(
-                                [seq[..seq.len() - 1].iter().collect(), tail_seq.clone()].concat(),
-                            );
-                            if r.len() == maxn {
-                                break;
-                            }
+            let mut to_visit = vec![Some((seq.last().unwrap(), node))];
+            while let Some(record) = to_visit.pop() {
+                if let Some((ch, leaf)) = record {
+                    to_visit.push(None);
+                    to_visit.extend(leaf.get_all_leaves().into_iter().map(|v| Some(v)));
+                    tail_seq.push(ch);
+                    if leaf.is_a_seq() {
+                        r.push([seq[..seq.len() - 1].iter().collect(), tail_seq.clone()].concat());
+                        if r.len() == maxn {
+                            break;
                         }
-                    } else {
-                        tail_seq.pop();
                     }
+                } else {
+                    tail_seq.pop();
                 }
             }
             return Some(r);
@@ -155,7 +134,7 @@ impl<T: Hash + Eq + Clone> TrieTree<T> {
         }
     }
 
-    pub fn prefix_match<'a>(&self, seq: &'a [T]) -> Option<Vec<Vec<&'a T>>> {
+    pub fn prefix_match<'a>(&'a self, seq: &'a [T]) -> Option<Vec<Vec<&'a T>>> {
         self.prefix_match_maxn(seq, usize::MAX)
     }
 }
--- src/tree/node.rs
+++ src/tree/node.rs
@@ -21,57 +21,66 @@ impl<T: Hash + Eq + Clone> Node<T> {
         }
     }
 
-    pub fn key_destroy(&mut self, ch: &T) {
-        self.child.remove(ch);
-    }
+    // pub fn key_destroy(&mut self, ch: &T) {
+    //     self.child.remove(ch);
+    // }
 
-    pub fn get_all_leaves_mut(&mut self) -> Vec<(&T, *mut Node<T>)> {
-        let mut r = Vec::<(&T, *mut Node<T>)>::new();
-        for (k, v) in self.child.iter_mut() {
-            r.push((k, &mut **v))
-        }
-        r
-    }
+    // pub fn get_all_leaves_mut(&mut self) -> Vec<(&T, &mut Box<Node<T>>)> {
+    //     self.child.iter_mut().collect()
+    // }
 
-    pub fn get_all_leaves(&self) -> Vec<(&T, *const Node<T>)> {
-        let mut r = Vec::<(&T, *const Node<T>)>::new();
-        for (k, v) in self.child.iter() {
-            r.push((k, &**v))
-        }
-        r
+    pub fn get_all_leaves(&self) -> Vec<(&T, &Box<Node<T>>)> {
+        self.child.iter().collect()
     }
 
     pub fn contains_key(&self, ch: &T) -> bool {
         self.child.contains_key(ch)
     }
 
-    pub fn key_next(&self, ch: &T) -> *const Node<T> {
-        let node = self.child.get(ch).unwrap();
-        return &**node;
+    pub fn key_next(&self, ch: &T) -> &Box<Node<T>> {
+        self.child.get(ch).unwrap()
     }
 
-    pub fn key_next_mut(&mut self, ch: &T) -> *mut Node<T> {
-        let node = self.child.get_mut(ch).unwrap();
-        return &mut **node;
+    pub fn key_next_mut(&mut self, ch: &T) -> &mut Box<Node<T>> {
+        self.child.get_mut(ch).unwrap()
     }
 
     pub fn key_alloc(&mut self, ch: &T) {
         self.child.insert(ch.clone(), Box::new(Node::new()));
     }
 
-    pub fn cannot_be_deleted(&self) -> bool {
-        self.is_a_seq() || self.child.keys().len() > 1
-    }
+    // pub fn cannot_be_deleted(self: &Box<Self>) -> bool {
+    //     let mut to_visit = vec![self];
+    //     while let Some(exp_node) = to_visit.pop() {
+    //         to_visit.extend(exp_node.get_all_leaves().into_iter().map(|(_, v)| v));
+    //         if exp_node.is_a_seq() {
+    //             return true;
+    //         }
+    //     }
+    //     false
+    // }
 
     pub fn is_a_seq(&self) -> bool {
         self.is_seq
     }
 
-    pub fn mark(&mut self) {
-        self.is_seq = true;
+    pub fn mark(&mut self) -> bool {
+        const TARGET_VAL: bool = true;
+        if self.is_seq == TARGET_VAL {
+            false
+        } else {
+            self.is_seq = TARGET_VAL;
+            true
+        }
     }
 
-    pub fn unmark(&mut self) {
-        self.is_seq = false;
+    pub fn unmark(&mut self) -> bool {
+        const TARGET_VAL: bool = false;
+        if self.is_seq == TARGET_VAL {
+            false
+        } else {
+            self.is_seq = TARGET_VAL;
+            true
+        }
     }
 }
