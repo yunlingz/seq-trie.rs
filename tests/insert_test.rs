b792c393bf65e5c002e229456bb44b721e375f7c
Ling CHU <meetchuling@gmail.com>
2020-06-22 21:44:37 +0800

    use ref for query

--- tests/insert_test.rs
+++ tests/insert_test.rs
@@ -2,24 +2,22 @@ use seq_trie;
 
 #[test]
 fn test_insert_0() {
-  let mut m = seq_trie::TrieTree::new();
-  let seq_dict = ["na", "no", "nie", "nte"];
-  let seq_dict: Vec<Vec<char>> =
-    seq_dict.iter().map(|seq| seq.chars().collect()).collect();
-  m.insert_iterator(seq_dict.clone().into_iter());
-  for seq in seq_dict.iter() {
-    assert!(m.contains(seq));
-  }
+    let mut m = seq_trie::TrieTree::new();
+    let seq_dict = ["na", "no", "nie", "nte"];
+    let seq_dict: Vec<Vec<char>> = seq_dict.iter().map(|seq| seq.chars().collect()).collect();
+    m.insert_iterator(seq_dict.clone().into_iter());
+    for seq in seq_dict.iter() {
+        assert!(m.contains(seq));
+    }
 }
 
 #[test]
 fn test_insert_1() {
-  let mut m = seq_trie::TrieTree::new();
-  let seq_dict = ["na", "no", "nie", "nte"];
-  let seq_dict: Vec<Vec<char>> =
-    seq_dict.iter().map(|seq| seq.chars().collect()).collect();
-  m.insert_iterator(seq_dict.clone().into_iter());
-  for seq in seq_dict.iter() {
-    assert!(m.contains(seq));
-  }
+    let mut m = seq_trie::TrieTree::new();
+    let seq_dict = ["na", "no", "nie", "nte"];
+    let seq_dict: Vec<Vec<char>> = seq_dict.iter().map(|seq| seq.chars().collect()).collect();
+    m.insert_iterator(seq_dict.clone().into_iter());
+    for seq in seq_dict.iter() {
+        assert!(m.contains(seq));
+    }
 }
